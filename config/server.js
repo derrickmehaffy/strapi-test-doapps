db45cfdd522502a5ce99fc399cd2f457587b6fb7
Derrick Mehaffy <derrickmehaffy@gmail.com>
2021-08-18 09:25:51 -0700

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd1c294569688a9d25f07e2aa771c510e'),
+    },
+  },
+});
